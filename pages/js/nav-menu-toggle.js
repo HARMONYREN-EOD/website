7c5506260870de026f4300c2cf46852459d09354
kai5041 <eod.contact-en@harmonyren.net>
2025-12-06 14:32:39 +0100

    Testing google analytics

--- pages/js/nav-menu-toggle.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const hamburger = document.querySelector('.mobile-only');
-  const navMenu = document.getElementById('nav-menu');
-
-  hamburger.addEventListener('click', () => {
-    navMenu.classList.toggle('active');
-  });
-});
